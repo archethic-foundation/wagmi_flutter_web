549bdcd63796af49ce59f84e142143fdd8c49572
Chralu <contact@charly-caulet.net>
2024-08-21 14:41:54 +0200

    refactor: :recycle: Improve script injection.

--- lib/wagmi.dart
+++ lib/wagmi.dart
@@ -1,35 +1,49 @@
 library wagmi_flutter_web;
 
-export 'src/wagmi_chain.dart';
-export 'src/wagmi_web3modal.dart';
 import 'dart:async';
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
-Future<void> init() async {
-  await injectJavascript('assets/main.js');
-}
+export 'src/wagmi_chain.dart';
+export 'src/wagmi_web3modal.dart';
 
-Future<void> injectJavascript(String assetPath) async {
-  const readyEventName = 'wagmi_flutter_web_ready';
+var _isReady = false;
+
+Future<void> init() async {
+  if (_isReady) return;
+
   final completer = Completer();
 
+  _completeOnReadyEvent(completer);
+
+  await injectJavascriptModule('assets/main.js');
+
+  _isReady = true;
+  return completer.future;
+}
+
+void _completeOnReadyEvent(Completer completer) {
+  const readyEventName = 'wagmi_flutter_web_ready';
+
   void readyEventListener(event) {
-    html.window.document
-        .removeEventListener(readyEventName, readyEventListener);
+    html.window.document.removeEventListener(
+      readyEventName,
+      readyEventListener,
+    );
     completer.complete();
   }
 
+  html.window.document.addEventListener(
+    readyEventName,
+    readyEventListener,
+  );
+}
+
+Future<void> injectJavascriptModule(String assetPath) async {
   final scriptPath = 'packages/wagmi_flutter_web/$assetPath';
 
   final scriptNode = html.ScriptElement()
     ..type = 'module'
     ..src = scriptPath;
   html.window.document.getElementsByTagName('html')[0].append(scriptNode);
-  html.window.document.addEventListener(
-    readyEventName,
-    readyEventListener,
-  );
-
-  return completer.future;
 }
